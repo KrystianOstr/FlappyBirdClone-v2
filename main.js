08d9f8be11255150b78c97a1560fc092e0a71c51
Krystian-W <krystian.ostrogorski@gmail.com>
2024-06-27 13:16:17 +0000

    added background

--- main.js
+++ main.js
@@ -1,24 +1,39 @@
-import './style.css'
-import javascriptLogo from './javascript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.js'
+import "./style.css";
+import Phaser from "phaser";
 
-document.querySelector('#app').innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
-  </div>
-`
+const config = {
+  type: Phaser.AUTO,
+  width: 800,
+  height: 600,
+  physics: {
+    default: "arcade",
+  },
+  scene: {
+    preload,
+    create,
+    update,
+  },
+};
 
-setupCounter(document.querySelector('#counter'))
+let bird = null;
+
+function preload() {
+  this.load.image("bg", "./assets/sky.png");
+  this.load.image("bird", "./assets/bird.png");
+}
+
+function create() {
+  // this.add.image(config.width / 2, config.height / 2, "bg"); <- the same like the one on the bottom
+  this.add.image(0, 0, "bg").setOrigin(0);
+  bird = this.physics.add
+    .sprite(config.width / 10, config.height / 2, "bird")
+    .setOrigin(0);
+
+  bird.body.velocity.y = 500;
+}
+
+function update() {
+  // console.log(`object`);
+}
+
+new Phaser.Game(config);
